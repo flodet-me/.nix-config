6aee00944022c3181a6df335bd8485e52eb6711b
flodet <develop@flodet.me>
2025-01-26 21:50:32 +0100

    wip

--- home/flodet/_shared/components/hyprland/default.nix
+++ home/flodet/_shared/components/hyprland/default.nix
@@ -1,22 +1,3 @@
 {
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-          9)
-      );
-  };
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
 }
