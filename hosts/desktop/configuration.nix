a869a997305f235638b18620b6c613f2727b09c5
flodet <develop@flodet.me>
2025-01-27 16:30:04 +0100

    wip docker

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -44,6 +44,27 @@
     "flakes"
   ];
 
+  environment.etc."blocky/config.yml".source = ./config.yml;
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      blocky = {
+        image = "spx01/blocky:latest";
+        ports = [
+          "53:53/tcp"
+          "53:53/udp"
+        ];
+        environment = {
+            TZ = "Europe/Berlin";
+        };
+        volumes = [
+           "/etc/blocky/config.yml:/app/config.yml"
+        ];
+      };
+    };
+  };
+
   programs.streamcontroller.enable = true;
   programs.zsh.enable = true;
 
