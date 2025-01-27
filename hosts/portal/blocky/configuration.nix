85dfb005e1f0cd56ee387cad724c7c3d908fc11e
flodet <develop@flodet.me>
2025-01-28 00:06:55 +0100

    wip

---
+++ hosts/portal/blocky/configuration.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+    environment.etc."blocky/config.yml".source = ./config.yml;
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      blocky = {
+        image = "spx01/blocky:latest";
+        ports = [
+          "53:53/tcp"
+          "53:53/udp"
+        ];
+        environment = {
+            TZ = "Europe/Berlin";
+        };
+        volumes = [
+           "/etc/blocky/config.yml:/app/config.yml"
+        ];
+      };
+    };
+  };
+
+}
