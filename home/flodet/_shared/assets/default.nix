2e8eefc5ae0cfab2bfca964a3afe427306e09813
flodet <develop@flodet.me>
2025-02-01 22:24:47 +0100

    wip

--- home/flodet/_shared/assets/default.nix
+++ home/flodet/_shared/assets/default.nix
@@ -2,7 +2,7 @@
     # Output filter for easyeffects
   # Source: https://github.com/JackHack96/EasyEffects-Presets
   home.file.".local/share/" = {
-    source = ./icons;
+    source = ./share;
     recursive = true;
   };
 }
