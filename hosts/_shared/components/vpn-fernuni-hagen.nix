db5ba9d5e68b1fb181408f610b3890daaedab76c
flodet <develop@flodet.me>
2025-02-02 22:33:21 +0100

    wip

--- hosts/_shared/components/vpn-fernuni-hagen.nix
+++ hosts/_shared/components/vpn-fernuni-hagen.nix
@@ -12,11 +12,20 @@
       connection = {
         id = "fernuni-hagen";
         type = "vpn";
+        autoconnect = "false";
       };
       vpn = {
+        authtype= "password";
         gateway = "vpn.fernuni-hagen.de";
         protocol = "anyconnect";
         service-type = "org.freedesktop.NetworkManager.openconnect";
+      };
+      ipv4 = {
+        method = "auto";
+      };
+      ipv6 = {
+        addr-gen-mode= "default";
+        method = "auto";
       };
     };
   };
